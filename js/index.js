849499aa0ed9c7824d4565b1f169132937b58ad8
GianlucaBitocchi <sechh@live.com.ar>
2023-06-13 19:40:20 -0300

    tercera entrega

--- js/index.js
+++ js/index.js
@@ -8,26 +8,26 @@ class PrestamoUsuario {
         this.monto_final = 0;
     }
 
-// calculador de intereses
+    // calculador de intereses
     calcularIntereses(monto, cuotas) {
 
         let interes;
 
         if (cuotas == 3) {
             interes = monto * 0.30;
-            this.monto_final = this.monto + interes;
+            this.montoFinal = this.monto + interes;
         }
         else if (cuotas == 6) {
             interes = monto * 0.60;
-            this.monto_final = this.monto + interes;
+            this.montoFinal = this.monto + interes;
         }
         else if (cuotas == 12) {
             interes = monto * 1.20;
-            this.monto_final = this.monto + interes;
+            this.montoFinal = this.monto + interes;
         }
         else if (cuotas == 18) {
             interes = monto * 1.80;
-            this.monto_final = this.monto + interes;
+            this.montoFinal = this.monto + interes;
         }
 
     }
@@ -35,46 +35,101 @@ class PrestamoUsuario {
     // datos del prestamo
     get_datos() {
 
-        console.log("Titular del prestamo: ", this.nombre);
         console.log("Monto solicitado: ", this.monto);
         console.log("Cantidad de cuotas solicitadas: ", this.cuotas);
-        console.log("Total a pagar con intereses: ", this.monto_final);
+        console.log("Total a pagar con intereses: ", this.montoFinal);
+        console.log("Total a pagar por cuota: ", this.montoFinal / this.cuotas)
 
     }
 }
 
-let nombreUsuario = "";
-let prestamos = [ ];
+let prestamos = [];
 
-for (let i = 0; i < 3; i = i + 1) {
+function calcular() {
 
-    let nombreUsuario = prompt("INGRESE SU NOMBRE Y APELLIDO");
-    let edad = prompt("INGRESE SU EDAD");
-    edad = parseInt(edad);
+    let formulario = document.getElementById("formulario");
 
-    if (edad >= 18) {
+    formulario.addEventListener("submit", function (calcular){
 
-        console.log("BIENVENIDO A PRESTAMOS RB:", nombreUsuario)
+        calcular.preventDefault();
 
-        let monto = prompt("INGRESE EL MONTO QUE DESEA SOLICITAR");
-        monto = parseInt(monto);
-        
-        let cuotas = prompt("EN CUANTAS CUOTAS 3 , 6 , 12 o 18");
-        cuotas = parseInt (cuotas);
+        let nombreUsuario = document.getElementById("nombreUsuario");
+        let edadUsuario = document.getElementById("edadUsuario");
+        edadUsuario = parseInt(edadUsuario);
+        let msj = document.getElementById("mensaje");
 
-        let prestamo = new PrestamoUsuario (nombreUsuario, monto, cuotas);
-        prestamo.calcularIntereses(monto, cuotas);
+        if (edadUsuario.value >= 18) {
 
-        prestamos.push(prestamo);
-        prestamo.get_datos ();
+            let parrafo = document.createElement("p");
 
-    }
+            parrafo.innerText = "BIENVENIDO A PRESTAMOS RB: " + nombreUsuario.value;
+            parrafo.style.textAlign = "center";
 
-    else if (edad <= 17) {
+            msj.append(parrafo);
+            console.log("nombre", nombreUsuario);
 
-        console.log(nombreUsuario ,"NECESITAS SER MAYOR DE EDAD");
-        break;
-    }
+            let monto = document.getElementById("montoUno");
+            monto = parseInt(monto);
+
+            let parrafoDos = document.createElement("p");
+
+            parrafoDos.innerText = "MONTO SOLICITADO: " + monto.value;
+            parrafoDos.style.textAlign = "center";
+
+            msj.append(parrafoDos);
+            console.log("monto", montoUno);
+
+            let cuotas = document.getElementById("cuotasUno");
+            cuotas = parseInt(cuotas);
+
+            let parrafoTres = document.createElement("p");
+
+            parrafoTres.innerText = "CANTIDAD DE CUOTAS SOLICITADAS: " + cuotas.value;
+            parrafoTres.style.textAlign = "center";
+
+            msj.append(parrafoTres);
+            console.log("cuotas", cuotasUno);
+
+            let parrafoCuatro = document.createElement("p");
+
+            parrafoCuatro.innerText = "TOTAL A DEVOLVER: " + montoFinal;
+            parrafoCuatro.style.textAlign = "center";
+
+            msj.append(parrafoCuatro);
+            console.log("monto final", montoFinal);
+
+            let parrafoCinco = document.createElement("p");
+
+            parrafoCinco.innerText = "TOTAL A PAGAR POR CUOTA: " + montoFinal / cuotas;
+            parrafoCinco.style.textAlign = "center";
+
+            msj.append(parrafoCinco);
+            console.log("monto total", montoFinal / cuotasUno);
+
+            let prestamo = new PrestamoUsuario(nombreUsuario, monto, cuotas);
+            prestamo.calcularIntereses(monto, cuotas);
+
+            prestamos.push(prestamo);
+            prestamo.get_datos();
+
+        }
+
+        else if (edadUsuario <= 17) {
+
+            document.body.innerHTML = `<h3>NECESITAS SER MAYOR DE EDAD </h3>
+                                                    <a href="index.html"> VOLVE A CALCULAR TU PRESTAMO </a>`;
+
+        }
+
+    })
 
 }
+
+let botonCalcular = document.getElementById("botonCalcular");
+botonCalcular.addEventListener("click", calcular);
+
+
+
+
+
 
