cc3d2bf8ac21c022b97c2861f0306abf489105de
GianlucaBitocchi <sechh@live.com.ar>
2023-07-04 09:11:29 -0300

    final

--- js/index.js
+++ js/index.js
@@ -1,64 +1,81 @@
 
-// calcular el prestamo
-function calcularPrestamo(e) {
+// Obtener los datos del formulario
+function obtenerDatosFormulario() {
+    return new Promise((resolve, reject) => {
+        let nombre = document.getElementById("nombre").value;
+        let edad = parseInt(document.getElementById("edad").value);
+        let monto = parseInt(document.getElementById("monto").value);
+        let cuotas = parseInt(document.getElementById("cuotas").value);
 
-    e.preventDefault();
+        if (nombre && !isNaN(edad) && !isNaN(monto) && !isNaN(cuotas)) {
+            resolve({ nombre, edad, monto, cuotas });
+        }
+        else {
+            reject("COMPLETA TODOS LOS DATOS");
+        }
+    });
 
-    // Obtener los valores del formulario
-    let nombre = document.getElementById("nombre").value;
-    let edad = parseInt(document.getElementById("edad").value);
-    let monto = parseInt(document.getElementById("monto").value);
-    let cuotas = parseInt(document.getElementById("cuotas").value);
+}
 
-    // corroborar edad usuario
-    if (edad >= 18) {
+    // Función error
+    function mostrarError(mensaje) {
+        Swal.fire({
+            icon: "error",
+            title: mensaje,
+            showClass: {
+                popup: "animate__animated animate__zoomIn",
+            },
+            hideClass: {
+                popup: "animate__animated animate__zoomOut",
+            },
+        });
+    }
 
-        // calcular el préstamo
-        let prestamo = new PrestamoUsuario(nombre, monto, cuotas);
-        prestamo.calcularIntereses();
-        let datosPrestamo = prestamo.obtenerDatos();
+    // calcular el prestamo
+    function calcularPrestamo(e) {
+        e.preventDefault();
 
-        // Mostrar los datos del dom
-        let resultadoP = document.getElementById("resultado");
-        resultadoP.style.display = "block";
-        resultadoP.style.textAlign = "center";
-        resultadoP.style.background = "rgb(218, 217, 214)";
-        resultadoP.innerHTML = `<h2>DATOS DEL PRESTAMO QUE SOLICITASTE</h2>
-                                                                    <h3>BIENVENIDO A PRESTAMOS RB : ${datosPrestamo.nombre}</h3>
+        obtenerDatosFormulario()
+            .then((datos) => {
+
+                // corroborar edad usuario
+                if (datos.edad >= 18) {
+
+                    // calcular el préstamo
+                    let prestamo = new PrestamoUsuario(datos.nombre, datos.monto, datos.cuotas);
+                    prestamo.calcularIntereses();
+                    let datosPrestamo = prestamo.obtenerDatos();
+
+                    // Mostrar los datos del dom
+                    let resultadoP = document.getElementById("resultado");
+                    resultadoP.style.display = "block";
+                    resultadoP.style.textAlign = "center";
+                    resultadoP.style.background = "rgb(218, 217, 214)";
+                    resultadoP.innerHTML = `<h2>DATOS DEL PRESTAMO QUE SOLICITADO</h2>
+                                                                    <h3>BIENVENIDO A PRESTAMOS RB   ${datosPrestamo.nombre}</h3>
                                                                     <h3>MONTO SOLICITADO : $${datosPrestamo.monto}</h3>
                                                                     <h3>MONTO A DEVOLVER : $${datosPrestamo.montoFinal.toFixed(2)}</h3>
                                                                     <h3>TOTAL A PAGAR POR MES : $${datosPrestamo.cuotaMensual.toFixed(2)}</h3>
                                                                     `;
 
-        let boton = document.getElementById("botonCalcular");
-        boton.addEventListener("click", calcularPrestamo);
+                    localStorage.setItem("datosPrestamo", JSON.stringify(datosPrestamo));
+                }
 
-        // Guardar los resultados 
-        localStorage.setItem("datosPrestamo", JSON.stringify(datosPrestamo));
-    }
+                else {
+                    mostrarError("NECESITAS SER MAYOR DE EDAD");
+                }
+            })
 
-    else {
-
-        Swal.fire({
-            icon: "error",
-            title: "NECESITAS SER MAYOR DE EDAD",
-            showClass: {
-                popup: "animate__animated animate__zoomIn"
-            },
-            hideClass: {
-                popup: "animate__animated animate__zoomOut"
-            }
-
-        });
+            .catch((error) => {
+                mostrarError(error);
+            });
 
     }
 
-}
+// evento calucular
+let boton = document.getElementById("botonCalcular");
+boton.addEventListener("click", calcularPrestamo);
 
-
-// evento submit del formulario
-let formulario = document.getElementById("formulario");
-formulario.addEventListener("submit", calcularPrestamo);
 
 // Obtener los datos del préstamo almacenados 
 document.addEventListener("DOMContentLoaded", function () {
@@ -76,5 +93,4 @@ document.addEventListener("DOMContentLoaded", function () {
     }
 
 });
-
 
